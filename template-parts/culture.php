e6f6554f474bb2adbdacebb9d29a89740aeba66e
Sebastian A Colombini <colombinis@gmail.com>
2025-03-30 22:54:28 -0300

    add culture secion mobile view (wip)

--- template-parts/culture.php
+++ template-parts/culture.php
@@ -1,6 +1,24 @@
 <section id="culture" class="culture">
-    <h2>Culture section</h2>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eos tenetur doloribus voluptatibus atque cumque, dicta dolorum necessitatibus! Dolorem consequuntur soluta fuga animi, distinctio exercitationem atque ad aperiam. Nostrum, obcaecati.
-    </p>
+	<div class="culture__wrapper">
+		<div class="culture__large-decoration culture-large-decoration" aria-hidden="true">
+			<span class="culture-large-decoration__text">002</span>
+		</div>
+		<div class="culture__media">
+			<div class="culture__media-main">
+				<picture class="culture__picture culture__picture-main">
+					<img src="src/images/culture/main.jpg" alt="" class="culture__image">
+				</picture>
+			</div>
+			<picture class="culture__picture culture__picture-top">
+				<img src="src/images/culture/top.jpg" alt="" class="culture__image">
+			</picture>
+		</div>
+		<div class="culture__content culture-content">
+			<header class="culture__header culture-header">
+				<span class="culture-header__subtitle">People making a difference</span>
+				<h1 class="culture-header__title">company culture</h1>
+			</header>
+			<p class="culture-content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia recusandae laudantium voluptatibus deleniti quis aliquid enim provident qui quisquam consequuntur a quaerat tenetur veritatis.</p>
+		</div>
+	</div>
 </section>
